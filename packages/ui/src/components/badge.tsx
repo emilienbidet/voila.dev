bd73aff11bcb6003ee7500286fd6a65a8b24d8b3
Emilien Bidet <emilienbdt@gmail.com>
2025-11-17 08:23:20 +0100

    ui.voila.dev: refactor Badge component to improve styling and structure, including updated variant definitions and enhanced props handling

--- packages/ui/src/components/badge.tsx
+++ packages/ui/src/components/badge.tsx
@@ -1,8 +1,16 @@
 import { useRender } from "@base-ui-components/react/use-render";
+import type * as React from "react";
+
 import { cva, cx, type VariantProps } from "..";
 
-export const variants = cva({
-	base: "inline-flex items-center justify-center rounded-full border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
+const variants = cva({
+	base:
+		"inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium " +
+		"w-fit whitespace-nowrap shrink-0 gap-1 overflow-hidden " +
+		"[&>svg]:size-3 [&>svg]:pointer-events-none " +
+		"focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] " +
+		"aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive " +
+		"transition-[color,box-shadow]",
 	variants: {
 		variant: {
 			default:
@@ -10,7 +18,8 @@ export const variants = cva({
 			secondary:
 				"border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
 			destructive:
-				"border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+				"border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 " +
+				"focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
 			outline:
 				"text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
 		},
@@ -20,15 +29,17 @@ export const variants = cva({
 	},
 });
 
-interface Props
-	extends useRender.ComponentProps<"span">,
-		VariantProps<typeof variants> {}
-
-export function Badge({ render, variant, className, ...props }: Props) {
+export function Badge({
+	className,
+	variant,
+	render = <span />,
+	...props
+}: React.ComponentProps<"span"> &
+	VariantProps<typeof variants> & { render?: useRender.RenderProp }) {
 	return useRender({
-		defaultTagName: "span",
 		render,
 		props: {
+			"data-slot": "badge",
 			className: cx(variants({ variant }), className),
 			...props,
 		},
